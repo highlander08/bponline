2d117a0fe7c012eb175eb8300ac69d49b0ffb020
highlandero8 <santosray62@gmail.com>
2020-04-21 20:09:34 -0300

    Fera

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ const path = require('path');
 
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io-client')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', (req,res) => {
+app.use('/', (req, res) => {
   res.render('index.html');
-});
+}); 
 
-let messagens = [];
+let messages = [];
 
 io.on('connection', socket => {
   console.log(`Socket Conectado: ${socket.id}`);
 
-  socket.emit('previousMessages', messagens);
+  socket.emit('previousMessages', messages);
 
   socket.on('sendMessage', data =>{
-    messagens.push(data);
-    socket.broadcast.emit('receive@Message', data);
+    messages.push(data);
+    socket.broadcast.emit('receivedMessage', data);
   });
 });
 
-server.listen(3000);
+server.listen(3333);
